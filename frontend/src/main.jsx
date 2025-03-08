680d167506dfe5a45954b6b6086ac740e26475e7
olawanlejoel <rahulrajesh2101@gmail.com>
2025-03-08 16:15:54 +0530

    fix routing.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,9 +9,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path='/test' element={<App/>}/>
+        <Route path='/' element={<App/>}/>
         
-        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
     </Router>
   </StrictMode>
